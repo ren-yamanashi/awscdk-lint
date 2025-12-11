e175db6dce8af090cc3d754e119885406da0fa04
ren-yamanashi <pinokionigg@icloud.com>
2025-12-11 09:53:19 +0900

    fix: build migration script

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "tsdown";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  outDir: "dist",
-  clean: true,
-  format: ["esm", "cjs"],
-  dts: true,
-  outputOptions: { exports: "named" },
-  treeshake: true,
-  fixedExtension: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    outDir: "dist",
+    clean: true,
+    format: ["esm", "cjs"],
+    dts: true,
+    outputOptions: { exports: "named" },
+    treeshake: true,
+    fixedExtension: true,
+  },
+  {
+    entry: ["scripts/migration/index.ts"],
+    outDir: "bin",
+    clean: true,
+    format: ["esm"],
+    dts: false,
+    platform: "node",
+    treeshake: true,
+    fixedExtension: true,
+  },
+]);
