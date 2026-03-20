d55b239f4167a936930379790e421442d841ee24
ren-yamanashi <pinokionigg@icloud.com>
2026-03-20 18:19:26 +0900

    upd

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -16,16 +16,5 @@ check_eslint_output() {
   echo "SUCCESS: Expected error count found!"
 }
 
-check_oxlint_output() {
-  local command="$1"
-  echo "RUNNING: $command"
-  if ! $command 2>&1 | grep -q "Found 0 warnings and 1 error."; then
-    echo "ERROR: Expected error count not found!"
-    exit 1
-  fi
-  echo "SUCCESS: Expected error count found!"
-}
-
 check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint lint:esm"
 check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint lint:cjs"
-check_oxlint_output "pnpm --filter @eslint-plugin-awscdk/example-oxlint exec oxlint"
