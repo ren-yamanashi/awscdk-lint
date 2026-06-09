2d8e35bbc8f58eba8f4a14038fa367d0b0534624
ren-yamanashi <pinokionigg@icloud.com>
2026-06-09 23:00:33 +0900

    chore: update examples structure

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -4,17 +4,38 @@ set -e
 
 cd "$(dirname "${0}")/.."
 
+EXPECTED_ERRORS=43
+
 check_eslint_output() {
   local command="$1"
   local output
   echo "RUNNING: $command"
   output=$($command 2>&1) || true
-  if ! echo "$output" | grep -q "✖ 43 problems (43 errors, 0 warnings)"; then
+  if ! echo "$output" | grep -q "✖ $EXPECTED_ERRORS problems ($EXPECTED_ERRORS errors, 0 warnings)";  then
     echo "ERROR: Expected error count not found!"
     exit 1
   fi
   echo "SUCCESS: Expected error count found!"
 }
 
-check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint lint:esm"
-check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint lint:cjs"
+check_oxlint_output() {
+  local command="$1"
+  local output
+  local count
+  # FIXME
+  EXPECTED_ERRORS=1
+  echo "RUNNING: $command"
+  output=$($command 2>&1) || true
+  count=$(echo "$output" | grep -c ": error ")
+  if [ "$count" -ne "$EXPECTED_ERRORS" ]; then
+    echo "ERROR: Expected $EXPECTED_ERRORS errors but found $count!"
+    echo "ACTUAL OUTPUT:"
+    echo "$output" | tail -5
+    exit 1
+  fi
+  echo "SUCCESS: Expected error count found!"
+}
+
+check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint eslint:esm"
+check_eslint_output "vp run -F @eslint-plugin-awscdk/example-eslint eslint:cjs"
+check_oxlint_output "vp run -F @eslint-plugin-awscdk/example-eslint oxlint"
