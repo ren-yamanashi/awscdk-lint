333a22b00a67d28beea963937e89b86e6e14bd13
ren-yamanashi <pinokionigg@icloud.com>
2026-06-20 19:42:43 +0900

    update

---
+++ packages/oxlint/src/__tests__/no-variable-construct-id.test.ts
@@ -0,0 +1,345 @@
+import { RuleTester } from "corsa-oxlint";
+
+import { noVariableConstructId } from "../rules/no-variable-construct-id";
+
+const ruleTester = new RuleTester({
+  languageOptions: { sourceType: "module" },
+});
+
+ruleTester.run("no-variable-construct-id", noVariableConstructId, {
+  valid: [
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, "SampleId");
+        }
+      }
+      `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, \`SampleId\`);
+        }
+      }
+      `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const items = ['a', 'b', 'c'];
+          for (const item of items) {
+            new TargetConstruct(this, item);
+          }
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const items = ['a', 'b', 'c'];
+          while (items.length > 0) {
+            new TargetConstruct(this, items.pop()!);
+          }
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const items = ['a', 'b', 'c'];
+          items.forEach(item => {
+            new TargetConstruct(this, item);
+          });
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const items = ['a', 'b', 'c'];
+          items.map(item => {
+            new TargetConstruct(this, item);
+            return item;
+          });
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          this.myMethod('id', 1);
+          this.myMethod('id', 2);
+          ['a', 'b', 'c'].map(item => this.myMethod(item, 3));
+        }
+        myMethod(id: string, num: number) {
+          return new TargetConstruct(this, id + num);
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const myArrowFunction = (id: string, num: number) => {
+            return new TargetConstruct(this, id + num);
+          };
+          myArrowFunction('id', 1);
+          myArrowFunction('id', 2);
+          ['a', 'b', 'c'].map(item => myArrowFunction(item, 3));
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        myArrowFunction = (id: string, num: number) => {
+          return new TargetConstruct(this, id + num);
+        };
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          this.myArrowFunction('id', 1);
+          this.myArrowFunction('id', 2);
+          ['a', 'b', 'c'].map(item => this.myArrowFunction(item, 3));
+        }
+      }
+    `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, validId: string) {
+          super(scope, validId);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, id);
+        }
+      }
+      `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, id);
+        }
+      }
+      `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      function createResource(scope: Construct, id: string) {
+        new TargetConstruct(scope, id);
+      }
+      `,
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class NotAConstruct {
+        create(scope: Construct, id: string) {
+          new TargetConstruct(scope, id);
+        }
+      }
+      `,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, id);
+        }
+      }
+      `,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string, props: { name: string}) {
+          super(scope, id);
+          new TargetConstruct(this, props.name);
+        }
+      }
+      `,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    {
+      code: [
+        "class Construct {}",
+        "class TargetConstruct extends Construct {",
+        "  constructor(scope: Construct, id: string) {",
+        "    super(scope, id);",
+        "  }",
+        "}",
+        "class SampleConstruct extends Construct {",
+        "  constructor(scope: Construct, id: string) {",
+        "    super(scope, id);",
+        "    new TargetConstruct(this, `${id}Bucket`);",
+        "  }",
+        "}",
+      ].join("\n"),
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, id + "Bucket");
+        }
+      }
+      `,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    {
+      code: `
+      const getId = () => "SampleId";
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new TargetConstruct(this, getId());
+        }
+      }
+      `,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    {
+      code: `
+      class Construct {}
+      class TargetConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      const id = "OutsideId";
+      const instance = new TargetConstruct(new Construct(), id);
+      `,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+  ],
+});
--- packages/oxlint/src/rules/index.ts
+++ packages/oxlint/src/rules/index.ts
@@ -4,6 +4,7 @@ import { noConstructStackSuffix } from "./no-construct-stack-suffix";
 import { noImportPrivate } from "./no-import-private";
 import { noMutablePropertyOfPropsInterface } from "./no-mutable-property-of-props-interface";
 import { noMutablePublicPropertyOfConstruct } from "./no-mutable-public-property-of-construct";
+import { noVariableConstructId } from "./no-variable-construct-id";
 import { pascalCaseConstructId } from "./pascal-case-construct-id";
 import { propsNameConvention } from "./props-name-convention";
 import { requireJSDoc } from "./require-jsdoc";
@@ -15,6 +16,7 @@ export const rules: Record<string, Rule> = {
   "no-import-private": noImportPrivate,
   "no-mutable-property-of-props-interface": noMutablePropertyOfPropsInterface,
   "no-mutable-public-property-of-construct": noMutablePublicPropertyOfConstruct,
+  "no-variable-construct-id": noVariableConstructId,
   "pascal-case-construct-id": pascalCaseConstructId,
   "props-name-convention": propsNameConvention,
   "require-jsdoc": requireJSDoc,
