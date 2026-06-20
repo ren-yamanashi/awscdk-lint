8a90de4e4b40e4df30776e39a8125d30c1b17d7b
ren-yamanashi <pinokionigg@icloud.com>
2026-06-20 20:21:23 +0900

    update

---
+++ packages/oxlint/src/__tests__/no-parent-name-construct-id-match.test.ts
@@ -0,0 +1,295 @@
+import { RuleTester } from "corsa-oxlint";
+
+import { noParentNameConstructIdMatch } from "../rules/no-parent-name-construct-id-match";
+
+const ruleTester = new RuleTester({
+  languageOptions: { sourceType: "module" },
+});
+
+ruleTester.run("no-parent-name-construct-id-match", noParentNameConstructIdMatch, {
+  valid: [
+    // WHEN: child id not same parent construct name
+    {
+      code: `
+      class Construct {}
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleConstruct(this, "ValidId");
+        }
+      }`,
+    },
+    // WHEN: child id not included parent construct name(typescript)
+    {
+      code: `
+      class Construct {}
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleConstruct(this, "Test-ValidId-Construct");
+        }
+      }`,
+    },
+    // WHEN: child id included parent construct name(typescript)
+    //       and disallowContainingParentName is false
+    {
+      code: `
+      class Construct {}
+      class SampleConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleConstruct(this, "SampleTestConstruct");
+        }
+      }`,
+      options: [{ disallowContainingParentName: false }],
+    },
+    // WHEN: instantiating class does not extend Construct
+    {
+      code: `
+      class Construct {}
+      class SampleClass {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestConstruct extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleClass(scope, "TestConstruct");
+        }
+      }`,
+    },
+    // WHEN: parent class does not extend Construct
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class SampleConstruct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleClass(scope, "TestConstruct");
+        }
+      }`,
+    },
+  ],
+  invalid: [
+    // WHEN: child class inside constructor (expression statement)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleClass(scope, "TestClass");
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+
+    // WHEN: child class inside constructor (expression statement)
+    //       and disallowContainingParentName is true
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          new SampleClass(scope, "SampleTestClass");
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+      options: [{ disallowContainingParentName: true }],
+    },
+
+    // WHEN: child class inside constructor (variable declaration)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          const test = new SampleClass(scope, "TestClass");
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside if statement inside constructor (expression statement)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          if (true) new SampleClass(scope, "TestClass");
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside if statement inside constructor (block statement)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          if (true) {
+            new SampleClass(scope, "TestClass");
+          }
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside if statement inside inside constructor (block statement / nested)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          if (true) {
+            if (true) {
+              new SampleClass(scope, "TestClass");
+            }
+          }
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside switch statement inside inside constructor (expression statement)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          switch (item.type) {
+            case "test":
+              const test = new SampleClass(scope, "TestClass");
+              break;
+          }
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside switch statement inside inside constructor (block statement)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          switch (item.type) {
+            case "test": {
+              const test = new SampleClass(scope, "TestClass");
+              break;
+            }
+          }
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: child statement inside switch statement inside inside constructor (block statement / nested)
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+          switch (item.type) {
+            case "test": {
+              switch (item.type) {
+                case "test":
+                  const test = new SampleClass(scope, "TestClass");
+                  break;
+              }
+            }
+          }
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+    // WHEN: in method
+    {
+      code: `
+      class Construct {}
+      class SampleClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+      }
+      class TestClass extends Construct {
+        constructor(scope: Construct, id: string) {
+          super(scope, id);
+        }
+        test() {
+          new SampleClass(scope, "TestClass");
+        }
+      }`,
+      errors: [{ messageId: "invalidConstructId" }],
+    },
+  ],
+});
--- packages/oxlint/src/rules/index.ts
+++ packages/oxlint/src/rules/index.ts
@@ -7,6 +7,7 @@ import { noConstructStackSuffix } from "./no-construct-stack-suffix";
 import { noImportPrivate } from "./no-import-private";
 import { noMutablePropertyOfPropsInterface } from "./no-mutable-property-of-props-interface";
 import { noMutablePublicPropertyOfConstruct } from "./no-mutable-public-property-of-construct";
+import { noParentNameConstructIdMatch } from "./no-parent-name-construct-id-match";
 import { noVariableConstructId } from "./no-variable-construct-id";
 import { pascalCaseConstructId } from "./pascal-case-construct-id";
 import { preferGrantsProperty } from "./prefer-grants-property";
@@ -24,6 +25,7 @@ export const rules: Record<string, Rule> = {
   "no-import-private": noImportPrivate,
   "no-mutable-property-of-props-interface": noMutablePropertyOfPropsInterface,
   "no-mutable-public-property-of-construct": noMutablePublicPropertyOfConstruct,
+  "no-parent-name-construct-id-match": noParentNameConstructIdMatch,
   "no-variable-construct-id": noVariableConstructId,
   "pascal-case-construct-id": pascalCaseConstructId,
   "prefer-grants-property": preferGrantsProperty,
