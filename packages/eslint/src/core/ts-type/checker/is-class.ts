f19892616e65aa07e054838b89de8b47c81c5f66
Ren Yamanashi <96362223+ren-yamanashi@users.noreply.github.com>
2026-09-02 19:05:59 +0900

    fix: detect declaration-merged CDK base classes in isClassType (#582)

--- packages/eslint/src/core/ts-type/checker/is-class.ts
+++ packages/eslint/src/core/ts-type/checker/is-class.ts
@@ -2,6 +2,14 @@ import { SymbolFlags, Type } from "typescript";
 
 import { getSymbol } from "./private/get-symbol";
 
+/**
+ * Checks whether the type is declared by a class
+ *
+ * NOTE: `SymbolFlags` is a bit field, so it must be tested with a mask.
+ * A class that is declaration-merged with an interface of the same name
+ * (as the generated CDK `*-augmentations.generated.d.ts` files do) carries
+ * `Interface` and `Transient` in addition to `Class`.
+ */
 export const isClassType = (type: Type): boolean => {
-  return getSymbol(type)?.flags === SymbolFlags.Class;
+  return ((getSymbol(type)?.flags ?? 0) & SymbolFlags.Class) !== 0;
 };
